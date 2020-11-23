a3735630ec1e26c5792ea5001d30735c98a1c498
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-11-23 17:49:06 +0100

    Passing file content.js

--- background.js
+++ background.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // grabAudioAndPlay()
     var button = document.getElementById("submit")
     var headings = document.getElementsByTagName("h2")
-    button.addEventListener("click", (e) => {
-      console.log("helloe", headings)
-      console.log(e)
+    button.addEventListener("click", (e) => {     
+      grabAudioAndPlay()
     })
 })
+
+function grabAudioAndPlay() {
+  console.log("me")
+  chrome.tabs.executeScript({ file: "content.js" })
+}
+
+
+
+
+
---
+++ content.js
@@ -0,0 +1,66 @@
+console.log("babbyyy")
+var synth = speechSynthesis;
+let voices = []
+
+const getVoices = () => {
+  synth.getVoices()
+  console.log(voices)
+}
+
+getVoices()
+if (synth.onvoiceschanged != undefined) {
+  synth.onvoiceschanged = getVoices()
+}
+
+const audio = () => {
+  console.log("execinggg")
+  // checks if speaking
+  if (synth.speaking) {
+    console.error("Already speaking..")
+    return
+  }
+
+  var text = getSelection()
+  // if selected != ''
+  console.log("herre", text)
+  const speakText = new SpeechSynthesisUtterance(text);
+  speakText.onend = e => {
+    console.log("Done speaking");
+  }
+
+  speakText.onerror = e => {
+    console.error("error playing text", e)
+  }
+
+  // speakText.voice = voices[1] 
+  // speakText.rate = 1
+
+  synth.speak(speakText)
+}
+
+const getSelection = () => {
+  var selectedText = ''; 
+  // window.getSelection 
+  if (window.getSelection) { 
+    selectedText = window.getSelection().toString(); 
+    console.log("here", selectedText) 
+  } 
+  // document.getSelection 
+  if (document.getSelection) {
+    selectedText = document.getSelection().toString(); 
+    console.log("here1", selectedText) 
+  } 
+  // document.selection 
+  else if (document.selection) { 
+    selectedText = document.selection.createRange().text; 
+    console.log("here2", selectedText)
+  } 
+
+  if (selectedText == "") {
+    selectedText = "No text selected"
+  }
+
+  return selectedText
+}
+
+// audio()
