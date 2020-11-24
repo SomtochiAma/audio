fb5033d7a735070c23a97e5a30af951402ff3da4
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-11-24 08:53:10 +0100

    Remove logs

--- content.js
+++ content.js
@@ -19,8 +19,8 @@ const audio = () => {
   }
 
   var text = getSelection()
-  // if selected != ''
   console.log("herre", text)
+
   const speakText = new SpeechSynthesisUtterance(text);
   speakText.onend = e => {
     console.log("Done speaking");
@@ -41,18 +41,15 @@ const getSelection = () => {
   // window.getSelection 
   if (window.getSelection) { 
     selectedText = window.getSelection().toString(); 
-    console.log("here", selectedText) 
   } 
   // document.getSelection 
   if (document.getSelection) {
     selectedText = document.getSelection().toString(); 
-    console.log("here1", selectedText) 
   } 
   // document.selection 
   else if (document.selection) { 
     selectedText = document.selection.createRange().text; 
-    console.log("here2", selectedText)
-  } 
+  } screenTop
 
   if (selectedText == "") {
     selectedText = "No text selected"
