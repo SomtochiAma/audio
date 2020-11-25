080de9c07c2c742de74f512531a865ccc3c148ff
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-11-25 18:58:29 +0100

    gets text from different context

--- background.js
+++ background.js
@@ -1,12 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const synth = window.speechSynthesis
-  console.log(synth)
-  var rate = document.querySelector("#rate")
-  var rateValue = document.querySelector("#rate-value")
-  var pitch = document.querySelector("#pitch")
-  var voiceSelect = document.querySelector("#voice-select")
+const synth = speechSynthesis
+let voices = []
 
-  let voices = []
+document.addEventListener("DOMContentLoaded", () => {
+  var voiceSelect = document.querySelector("#voice-select")
 
   const getVoices = () => {
     voices = synth.getVoices()
@@ -19,7 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
       option.setAttribute('data-name', voice.name)
       voiceSelect.appendChild(option)
     })
-    console.log("here")
   }
 
   // getVoices()
@@ -30,8 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
   setupFunction()
   var button = document.getElementById("submit")
   button.addEventListener("click", (e) => {   
-    selectedVoice = voiceSelect.options[voiceSelect.selectedIndex].dataset.name
-    grabAudioAndPlay(selectedVoice, rate.value, pitch.value)
+    grabAudioAndPlay()
   })
 })
 
@@ -40,10 +34,49 @@ function setupFunction() {
 }
 
 
-function grabAudioAndPlay(voice, rate, pitch) {
-  chrome.tabs.executeScript({ code: `audio("${voice}", ${rate}, ${pitch})`})
+// function grabAudioAndPlay(voice, rate, pitch) {
+//   chrome.tabs.executeScript({ code: `audio("${voice}", ${rate}, ${pitch})`})
+// }
+
+function grabAudioAndPlay() {
+  chrome.tabs.executeScript({ code: "getSelection()"}, playAudio)
+}
+
+function playAudio(results) {
+  var rate = document.querySelector("#rate").value
+  var pitch = document.querySelector("#pitch").value
+  var voiceSelect = document.querySelector("#voice-select")
+  var selectedVoiceName = voiceSelect.options[voiceSelect.selectedIndex].dataset.name
+  var selectedVoice
+  voices.forEach(voice => {
+    if (voice.name == selectedVoiceName) {
+      selectedVoice = voice
+    }
+  })
+
+  audio(results[0], selectedVoice, rate, pitch)
 }
 
 
+const audio = (text, voice, rate, pitch) => {
+  console.log(text)
+  if (synth.speaking) {
+    console.error("Already speaking..")
+    synth.cancel()
+  }
 
+  const speakText = new SpeechSynthesisUtterance(text);
+  speakText.onend = e => {
+    console.log("Done speaking");
+  }
 
+  speakText.onerror = e => {
+    console.error("error playing text", e)
+  }
+
+  speakText.rate = rate
+  speakText.pitch = pitch
+  speakText.voice = voice
+   
+  synth.speak(speakText)
+}
