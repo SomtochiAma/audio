12a790f237d9151fdb3bcdb8cafa87ba43456ca6
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-11-25 18:59:52 +0100

    Revert "gets text from different context"
    This reverts commit 080de9c07c2c742de74f512531a865ccc3c148ff.

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
-const synth = speechSynthesis
-let voices = []
-
 document.addEventListener("DOMContentLoaded", () => {
+  const synth = window.speechSynthesis
+  console.log(synth)
+  var rate = document.querySelector("#rate")
+  var rateValue = document.querySelector("#rate-value")
+  var pitch = document.querySelector("#pitch")
   var voiceSelect = document.querySelector("#voice-select")
+
+  let voices = []
 
   const getVoices = () => {
     voices = synth.getVoices()
@@ -15,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       option.setAttribute('data-name', voice.name)
       voiceSelect.appendChild(option)
     })
+    console.log("here")
   }
 
   // getVoices()
@@ -25,7 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
   setupFunction()
   var button = document.getElementById("submit")
   button.addEventListener("click", (e) => {   
-    grabAudioAndPlay()
+    selectedVoice = voiceSelect.options[voiceSelect.selectedIndex].dataset.name
+    grabAudioAndPlay(selectedVoice, rate.value, pitch.value)
   })
 })
 
@@ -34,49 +40,10 @@ function setupFunction() {
 }
 
 
-// function grabAudioAndPlay(voice, rate, pitch) {
-//   chrome.tabs.executeScript({ code: `audio("${voice}", ${rate}, ${pitch})`})
-// }
-
-function grabAudioAndPlay() {
-  chrome.tabs.executeScript({ code: "getSelection()"}, playAudio)
-}
-
-function playAudio(results) {
-  var rate = document.querySelector("#rate").value
-  var pitch = document.querySelector("#pitch").value
-  var voiceSelect = document.querySelector("#voice-select")
-  var selectedVoiceName = voiceSelect.options[voiceSelect.selectedIndex].dataset.name
-  var selectedVoice
-  voices.forEach(voice => {
-    if (voice.name == selectedVoiceName) {
-      selectedVoice = voice
-    }
-  })
-
-  audio(results[0], selectedVoice, rate, pitch)
+function grabAudioAndPlay(voice, rate, pitch) {
+  chrome.tabs.executeScript({ code: `audio("${voice}", ${rate}, ${pitch})`})
 }
 
 
-const audio = (text, voice, rate, pitch) => {
-  console.log(text)
-  if (synth.speaking) {
-    console.error("Already speaking..")
-    synth.cancel()
-  }
 
-  const speakText = new SpeechSynthesisUtterance(text);
-  speakText.onend = e => {
-    console.log("Done speaking");
-  }
 
-  speakText.onerror = e => {
-    console.error("error playing text", e)
-  }
-
-  speakText.rate = rate
-  speakText.pitch = pitch
-  speakText.voice = voice
-   
-  synth.speak(speakText)
-}
--- content.js
+++ content.js
@@ -1,3 +1,35 @@
+var synth = speechSynthesis;
+
+const audio = (voiceName, rate, pitch) => {
+  if (synth.speaking) {
+    console.error("Already speaking..")
+    return
+  }
+
+  var text = getSelection()
+  console.log("herre", text)
+
+  const speakText = new SpeechSynthesisUtterance(text);
+  speakText.onend = e => {
+    console.log("Done speaking");
+  }
+
+  speakText.onerror = e => {
+    console.error("error playing text", e)
+  }
+
+  speakText.rate = rate
+  speakText.pitch = pitch
+  
+  window.speechSynthesis.getVoices().forEach(voice => {
+    if (voice.name == voiceName) {
+      speakText.voice = voice
+    }
+  })
+   
+  synth.speak(speakText)
+}
+
 const getSelection = () => {
   var selectedText = ''; 
   // window.getSelection 
